ea28d780b02fe859777d6bd00027bb9cbc4b711e
Youssouf Sergma <sil3nthilldz@gmail.com>
2023-08-15 14:45:47 +0100

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form =document.getElementById("create-task-form")
+  form.preventDefault;
+  const forText=document.getElementById("new-task-description");
+  const button=document.getElementById("subbutton");
+  const tasks=document.getElementById("tasks");
+   button.addEventListener("click",(e) => {
+    e.preventDefault()
+    tasks.innerHTML = `<li>${forText}</li>`
+    console.log(forText)
+    form.preventDefault;
+    tasks.preventDefault;  
+   })
 });
